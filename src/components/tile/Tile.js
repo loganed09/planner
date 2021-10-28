e7462f215b894325393aed7d6b9f886e0917b240
Logan <loganed09@gmail.com>
2021-10-28 14:50:11 -0400

    Modified Tile.js

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({props}) => {
+  let propValues = Object.values(props);
   return (
     <div className="tile-container">
-      
+      {
+        propValues.map((value, index) => (
+          <p key={index} className={`${index === 0 ? "tile-title" : ""} tile`}>
+            {value}
+          </p>
+        ))
+      }
     </div>
   );
 };
